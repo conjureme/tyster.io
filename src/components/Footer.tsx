4ffaff75b3bf6c8b2d61820e40ca57fe04a33d3c
tyler <209597026+conjureme@users.noreply.github.com>
2025-06-07 00:07:28 -0500

    Finally add a footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,29 @@
+'use client';
+
+import { Icon } from '@iconify/react';
+
 export default function Footer() {
+  const currentYear = new Date().getFullYear();
+
   return (
-    <footer className='py-6 px-6 text-center text-gray-500 text-sm'>
-      <p>this will be a footer eventually! yipee!</p>
+    <footer className='mt-32 pb-8'>
+      <div className='mx-auto pt-16'>
+        <div className='max-w-6xl mx-auto'>
+          <div className='text-center space-y-2'>
+            <p className='text-sm text-gray-500'>
+              made with{' '}
+              <span className='inline-block animate-pulse'>
+                <Icon
+                  icon='mdi:heart'
+                  className='text-red-500 inline'
+                  width={16}
+                />
+              </span>
+            </p>
+            <p className='text-xs text-gray-400'>© {currentYear} tyster.io</p>
+          </div>
+        </div>
+      </div>
     </footer>
   );
 }
