9be7414172a45c4df61ce57d03e1d9009d1105d6
tyler <209597026+conjureme@users.noreply.github.com>
2025-06-24 22:10:17 -0500

    Fix spacing

--- src/app/(extra)/page.tsx
+++ src/app/(extra)/page.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <div className='min-h-screen flex flex-col bg-base-100'>
-      <div className='flex-1 pt-20 p-6 md:pt-24 md:p-12 md:flex md:items-center '>
+      <div className='flex-1 pt-24 p-6 md:p-0 md:flex md:items-center mx-auto max-w-6xl'>
         <div className='max-w-6xl'>
           <div className='space-y-8'>
             <section>
