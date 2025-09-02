23ec7819824dbc8e8f63d241509f86b8fc2adb20
tyler <209597026+conjureme@users.noreply.github.com>
2025-09-02 01:44:40 -0500

    Docs: installation guide

--- .source/index.ts
+++ .source/index.ts
@@ -1,5 +1,6 @@
 // @ts-nocheck -- skip type checking
+import * as docs_1 from "../content/moemoe/docs/discord/installation.mdx?collection=docs&hash=1756506870467"
 import * as docs_0 from "../content/moemoe/docs/discord/index.mdx?collection=docs&hash=1756506870467"
 import { _runtime } from "fumadocs-mdx"
 import * as _source from "../source.config"
-export const docs = _runtime.docs<typeof _source.docs>([{ info: {"path":"discord\\index.mdx","absolutePath":"C:/Users/zc1an/Desktop/github/tyster.io/content/moemoe/docs/discord/index.mdx"}, data: docs_0 }], [{"info":{"path":"discord\\meta.json","absolutePath":"C:/Users/zc1an/Desktop/github/tyster.io/content/moemoe/docs/discord/meta.json"},"data":{"title":"moemoe discord docs","pages":["---introductions---","index"],"description":"docs for the moemoe discord module","root":true}}])
+export const docs = _runtime.docs<typeof _source.docs>([{ info: {"path":"discord\\index.mdx","absolutePath":"C:/Users/zc1an/Desktop/github/tyster.io/content/moemoe/docs/discord/index.mdx"}, data: docs_0 }, { info: {"path":"discord\\installation.mdx","absolutePath":"C:/Users/zc1an/Desktop/github/tyster.io/content/moemoe/docs/discord/installation.mdx"}, data: docs_1 }], [{"info":{"path":"discord\\meta.json","absolutePath":"C:/Users/zc1an/Desktop/github/tyster.io/content/moemoe/docs/discord/meta.json"},"data":{"title":"moemoe discord docs","pages":["---introductions---","index","installation"],"description":"docs for the moemoe discord module","root":true}}])
